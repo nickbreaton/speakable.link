de8dfde5d39e910344170843ef6896e08864f607
Nick Breaton <nick@breaton.com>
2021-01-24 19:58:19 -0500

    Install postcss cli

--- packages/front-end/snowpack.config.js
+++ packages/front-end/snowpack.config.js
@@ -3,14 +3,16 @@
  */
 module.exports = {
     buildOptions: {
-        baseUrl: "https://speakable-link.firebaseapp.com/"
+        baseUrl: "https://speakable-link.web.app/"
     },
     plugins: [
         "@snowpack/plugin-typescript",
         "@snowpack/plugin-svelte",
         "@snowpack/plugin-postcss",
-        "@snowpack/plugin-optimize",
-        ["snowpack-plugin-content-hash", { exts: ['.css', '.js'] }],
+        [
+            "@snowpack/plugin-webpack",
+            { outputPattern: { js: "_app/[name].[hash].js", css: "_app/[name].[hash].css" } }
+        ]
     ],
     routes: [
         { match: "routes", src: ".*", dest: "/_app/index.html" }
